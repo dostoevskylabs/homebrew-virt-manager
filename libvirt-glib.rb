fd6c2d23fbf8be72b16fcd32af3f3e1c93ef0389
Elijah <dostoevskylabs@users.noreply.github.com>
2018-07-08 06:23:56 -0400

    Update libvirt-glib.rb

--- libvirt-glib.rb
+++ libvirt-glib.rb
@@ -1,4 +1,4 @@
-class LibvirtGLib < Formula
+class LibvirtGlib < Formula
   desc "Libvirt API for glib-based programs"
   homepage "https://libvirt.org/"
   url "https://libvirt.org/sources/glib/libvirt-glib-1.0.0.tar.gz"
